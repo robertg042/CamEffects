1aa041c9a432c4846f969085a578d8515d2d5538
Robert Górnicki <github@gorki.net.pl>
2018-05-22 11:28:01 +0200

    Video streaming added

--- src/app.js
+++ src/app.js
@@ -1,1 +1,12 @@
 import './app.scss';
+
+const video = document.getElementById("camera");
+
+navigator.mediaDevices.getUserMedia({video: true, audio: false})
+  .then(stream => {
+    video.srcObject = stream;
+    video.play();
+  })
+  .catch(err => {
+    console.log(err);
+  });
