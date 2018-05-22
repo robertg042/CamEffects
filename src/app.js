4035d13222011a60afa2db3e2c16693fc0d33117
Robert Górnicki <github@gorki.net.pl>
2018-05-22 21:06:10 +0200

    Snaping photo from webcam stream added

--- src/app.js
+++ src/app.js
@@ -1,6 +1,10 @@
 import './app.scss';
 
+let width = 500, height = 0, streaming = false;
+
 const video = document.getElementById("camera");
+const canvas = document.getElementById("canvas");
+const snapButton = document.getElementById("snap");
 
 navigator.mediaDevices.getUserMedia({video: true, audio: false})
   .then(stream => {
@@ -10,3 +14,30 @@ navigator.mediaDevices.getUserMedia({video: true, audio: false})
   .catch(err => {
     console.log(err);
   });
+
+video.addEventListener("canplay", event => {
+  if(!streaming) {
+    height = video.videoHeight / (video.videoWidth / width);
+    video.setAttribute("width", width);
+    video.setAttribute("height", height);
+    canvas.setAttribute("width", width);
+    canvas.setAttribute("height", height);
+
+    streaming = true
+  }
+}, false);
+
+snapButton.addEventListener("click", event => {
+  snapPhoto();
+
+  event.preventDefault();
+}, false);
+
+const snapPhoto = () => {
+  const context = canvas.getContext("2d");
+  if (width && height) {
+    canvas.width = width;
+    canvas.height = height;
+    context.drawImage(video, 0, 0, width, height);
+  }
+};
